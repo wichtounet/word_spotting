98a8ea37d2847940aebb20946c0f17fc5ac74f8e
Baptiste Wicht <baptiste.wicht@gmail.com>
2015-03-06 16:24:06 +0100

    Experimenting

--- src/spotter.cpp
+++ src/spotter.cpp
@@ -10,11 +10,16 @@
 
 #include "dll/conv_rbm.hpp"
 #include "dll/conv_rbm_mp.hpp"
+#include "dll/dbn.hpp"
+#include "dll/avgp_layer.hpp"
+#include "dll/mp_layer.hpp"
 
 #include "cpp_utils/parallel.hpp"
 
 #include "config.hpp"
 #include "washington.hpp"
+
+using weight = double;
 
 static constexpr const std::size_t WIDTH = 660;
 static constexpr const std::size_t HEIGHT = 120;
@@ -39,7 +44,7 @@ std::ostream& operator<<(std::ostream& stream, const std::vector<T>& vec){
 
 namespace {
 
-etl::dyn_matrix<double> mat_to_dyn(const config& conf, cv::Mat& image){
+etl::dyn_matrix<weight> mat_to_dyn(const config& conf, cv::Mat& image){
     cv::Mat normalized(cv::Size(WIDTH, HEIGHT), CV_8U);
     normalized = cv::Scalar(255);
 
@@ -51,7 +56,7 @@ etl::dyn_matrix<double> mat_to_dyn(const config& conf, cv::Mat& image){
         cv::adaptiveThreshold(scaled_normalized, normalized, 255, CV_ADAPTIVE_THRESH_MEAN_C, CV_THRESH_BINARY, 7, 2);
     }
 
-    etl::dyn_matrix<double> training_image(normalized.size().width, normalized.size().height);
+    etl::dyn_matrix<weight> training_image(normalized.size().width, normalized.size().height);
 
     for(std::size_t y = 0; y < static_cast<std::size_t>(normalized.size().height); ++y){
         for(std::size_t x = 0; x < static_cast<std::size_t>(normalized.size().width); ++x){
@@ -128,40 +133,39 @@ int command_train(const config& conf){
     std::cout << valid_image_names.size() << " validation word images in set" << std::endl;
     std::cout << test_image_names.size() << " test word images in set" << std::endl;
 
-    std::vector<etl::dyn_matrix<double>> training_images;
+    std::vector<etl::dyn_matrix<weight>> training_images;
+
+    train_image_names.resize(300);
+    test_image_names.resize(300);
 
     for(auto& name : train_image_names){
-        if(training_images.size() < 2400){
-            training_images.emplace_back(mat_to_dyn(conf, dataset.word_images[name]));
-        }
+        training_images.emplace_back(mat_to_dyn(conf, dataset.word_images[name]));
     }
 
     auto evaluate = [&](auto& crbm){
-        std::array<double, MAX_N + 1> recalls;
-        std::array<double, MAX_N + 1> precisions;
+        std::array<double, MAX_N + 1> tp;
         std::array<double, MAX_N + 1> fn;
 
-        std::fill(recalls.begin(), recalls.end(), 0);
-        std::fill(precisions.begin(), precisions.end(), 0);
+        std::fill(tp.begin(), tp.end(), 0);
         std::fill(fn.begin(), fn.end(), 0);
 
         std::cout << "Compute features on the test set..." << std::endl;
 
-        std::vector<etl::dyn_matrix<double, 3>> test_features_a;
-        std::vector<etl::dyn_matrix<double, 3>> test_features_s;
+        std::vector<etl::dyn_matrix<weight, 3>> test_features_a;
+        //std::vector<etl::dyn_matrix<weight, 3>> test_features_s;
 
         for(std::size_t i = 0; i < test_image_names.size(); ++i){
-            test_features_a.push_back(crbm->prepare_one_output());
-            test_features_s.push_back(crbm->prepare_one_output());
+            test_features_a.emplace_back(crbm->prepare_one_output());
+            //test_features_s.push_back(crbm->prepare_one_output());
         }
 
         cpp::default_thread_pool<> pool;
 
         cpp::parallel_foreach_i(pool, test_image_names.begin(), test_image_names.end(),
-            [&test_features_a, &test_features_s, &crbm, &dataset, &conf](auto& test_image, std::size_t i){
+            [&test_features_a, /*&test_features_s, */&crbm, &dataset, &conf](auto& test_image, std::size_t i){
                 auto test_v = mat_to_dyn(conf, dataset.word_images[test_image]);
 
-                crbm->activate_one(test_v, test_features_a[i], test_features_s[i]);
+                crbm->activation_probabilities(test_v, test_features_a[i]);
             });
 
         std::cout << "... done" << std::endl;
@@ -179,7 +183,18 @@ int command_train(const config& conf){
                 }
             }
 
+            //Make sure that there is a sample in the training set
             if(training_image.empty()){
+                std::cout << "Skipped " << keyword << " since there are no example in the training set" << std::endl;
+                continue;
+            }
+
+            auto total_positive = std::count_if(test_image_names.begin(), test_image_names.end(),
+                [&dataset, &keyword](auto& i){ return dataset.word_labels[{i.begin(), i.end() - 4}] == keyword; });
+
+            //Make sure that there is a sample in the test set
+            if(total_positive == 0){
+                std::cout << "Skipped " << keyword << " since there are no example in the test set" << std::endl;
                 continue;
             }
 
@@ -187,46 +202,42 @@ int command_train(const config& conf){
 
             auto ref_v = mat_to_dyn(conf, dataset.word_images[training_image + ".png"]);
             auto ref_a = crbm->prepare_one_output();
-            auto ref_s = crbm->prepare_one_output();
+            //auto ref_s = crbm->prepare_one_output();
 
-            crbm->activate_one(ref_v, ref_a, ref_s);
+            crbm->activation_probabilities(ref_v, ref_a);
 
-            std::vector<std::pair<std::string, double>> diffs_a;
-            std::vector<std::pair<std::string, double>> diffs_s;
+            std::vector<std::pair<std::string, weight>> diffs_a;
+            //std::vector<std::pair<std::string, weight>> diffs_s;
 
             for(std::size_t t = 0; t < test_image_names.size(); ++t){
                 decltype(auto) test_image = test_image_names[t];
 
-                double diff_a = etl::sum(etl::abs(ref_a - test_features_a[t]));
+                auto diff_a = etl::sum(etl::abs(ref_a - test_features_a[t]));
                 diffs_a.emplace_back(std::string(test_image.begin(), test_image.end() - 4), diff_a);
 
-                double diff_s = etl::sum(etl::abs(ref_s - test_features_s[t]));
-                diffs_s.emplace_back(std::string(test_image.begin(), test_image.end() - 4), diff_s);
+                //auto diff_s = etl::sum(etl::abs(ref_s - test_features_s[t]));
+                //diffs_s.emplace_back(std::string(test_image.begin(), test_image.end() - 4), diff_s);
             }
 
             std::sort(diffs_a.begin(), diffs_a.end(), [](auto& a, auto& b){ return a.second < b.second; });
-            std::sort(diffs_s.begin(), diffs_s.end(), [](auto& a, auto& b){ return a.second < b.second; });
+            //std::sort(diffs_s.begin(), diffs_s.end(), [](auto& a, auto& b){ return a.second < b.second; });
 
-            auto total_positive = std::count_if(diffs_a.begin(), diffs_a.end(),
-                [&dataset, &keyword](auto& d){ return dataset.word_labels[d.first] == keyword; });
-
-            std::cout << "Best diff(a):" << diffs_a.front().second << std::endl;
-            std::cout << "Worst diff(a):" << diffs_a.back().second << std::endl;
-            std::cout << "Best diff(s):" << diffs_s.front().second << std::endl;
-            std::cout << "Worst diff(s):" << diffs_s.back().second << std::endl;
+            //std::cout << "Best diff(a):" << diffs_a.front().second << std::endl;
+            //std::cout << "Worst diff(a):" << diffs_a.back().second << std::endl;
+            //std::cout << "Best diff(s):" << diffs_s.front().second << std::endl;
+            //std::cout << "Worst diff(s):" << diffs_s.back().second << std::endl;
 
             for(std::size_t n = 1; n <= MAX_N; ++n){
-                int tp = 0;
+                int tp_n = 0;
 
                 for(std::size_t i = 0; i < n && i < diffs_a.size(); ++i){
                     if(dataset.word_labels[diffs_a[i].first] == keyword){
-                        ++tp;
+                        ++tp_n;
                     }
                 }
 
-                recalls[n] += tp;
-                precisions[n] += tp;
-                fn[n] += total_positive - tp;
+                tp[n] += tp_n;
+                fn[n] += total_positive - tp_n;
             }
         }
 
@@ -235,34 +246,90 @@ int command_train(const config& conf){
         std::cout << evaluated << " keywords evaluated" << std::endl;
 
         for(std::size_t n = 1; n <= MAX_N; ++n){
-            std::cout << "Precision(" << n << ") = " << (precisions[n] / (n * set.keywords.size())) << std::endl;
-            std::cout << "Recall(" << n << ") = " << (recalls[n] / fn[n]) << std::endl;
+            std::cout << "Precision(" << n << ") = " << (tp[n] / (n * set.keywords.size())) << std::endl;
+            std::cout << "Recall(" << n << ") = " << (tp[n] / (tp[n] + fn[n])) << std::endl;
         }
     };
 
     if(conf.half){
-        using crbm_t =
-            dll::conv_rbm_desc<
-            WIDTH / 2, HEIGHT / 2, 1                    //330x60 input image (1 channel)
-            , WIDTH / 2 + 1 - 19 , HEIGHT / 2 + 1 - 19  //Configure the size of the filter
-            , 12                                        //Number of feature maps
-            //, 2                                       //Probabilistic max pooling (2x2)
-            , dll::batch_size<25>
-            , dll::parallel
-            , dll::verbose
-            , dll::momentum
-            , dll::weight_decay<dll::decay_type::L2>
-            >::rbm_t;
+        static constexpr const std::size_t NF = 13;
+        static constexpr const std::size_t NF2 = 8;
+        static constexpr const std::size_t NF3 = 5;
 
-        auto crbm = std::make_unique<crbm_t>();
+        using cdbn_t =
+            dll::dbn_desc<
+                dll::dbn_layers<
+                    dll::conv_rbm_desc<
+                        WIDTH / 2, HEIGHT / 2, 1                    //330x60 input image (1 channel)
+                        , WIDTH / 2 + 1 - NF , HEIGHT / 2 + 1 - NF  //Configure the size of the filter
+                        , 96                                       //Number of feature maps
+                        //, 2                                       //Probabilistic max pooling (2x2)
+                        , dll::weight_type<weight>
+                        , dll::batch_size<25>
+                        , dll::parallel
+                        , dll::verbose
+                        , dll::momentum
+                        , dll::weight_decay<dll::decay_type::L2>
+                        //, dll::sparsity<dll::sparsity_method::LEE>
+                    >::rbm_t
+                    , dll::mp_layer_3d_desc<96,318,48,1,2,2>::layer_t
+                    , dll::conv_rbm_desc<
+                        159, 24, 96
+                        , 159 + 1 - NF2 , 24 + 1 - NF2
+                        , 64
+                        , dll::weight_type<weight>
+                        , dll::batch_size<25>
+                        , dll::parallel
+                        , dll::verbose
+                        , dll::momentum
+                        , dll::weight_decay<dll::decay_type::L2>
+                        //, dll::sparsity<dll::sparsity_method::LEE>
+                    >::rbm_t
+                    , dll::conv_rbm_desc<
+                        152, 17, 64
+                        , 152 + 1 - NF3 , 17 + 1 - NF3
+                        , 32
+                        , dll::weight_type<weight>
+                        , dll::batch_size<25>
+                        , dll::parallel
+                        , dll::verbose
+                        , dll::momentum
+                        , dll::weight_decay<dll::decay_type::L2>
+                        //, dll::sparsity<dll::sparsity_method::LEE>
+                    >::rbm_t
+                >
+            >::dbn_t;
 
-        std::cout << crbm->output_size() << " output features" << std::endl;
+        auto cdbn = std::make_unique<cdbn_t>();
 
-        crbm->train(training_images, 5);
-        crbm->store("method_1_half.dat");
-        //crbm->load("method_1.dat");
+        cdbn->display();
 
-        evaluate(crbm);
+        std::cout << cdbn->output_size() << " output features" << std::endl;
+
+        auto mini_div = (1024.0 * 1024.0);
+        auto div = sizeof(std::size_t) / (mini_div);
+        auto mul = training_images.size() * div;
+
+        std::cout << "DBN size: " << sizeof(cdbn_t) * div << "MB" << std::endl;
+
+        std::cout << "Layer 0 tmp storage: " << cdbn->template layer<0>().output_size() * mul << "MB necessary" << std::endl;
+        std::cout << "Layer 1 tmp storage: " << cdbn->template layer<1>().output_size() * mul << "MB necessary" << std::endl;
+        std::cout << "Layer 2 tmp storage: " << cdbn->template layer<2>().output_size() * mul << "MB necessary" << std::endl;
+        std::cout << "Layer 3 tmp storage: " << cdbn->template layer<3>().output_size() * mul << "MB necessary" << std::endl;
+
+        std::cout << "Trainer 0 storage: " << sizeof(dll::base_cd_trainer<1, cdbn_t::rbm_type<0>, false, false>) / mini_div << "MB necessary" << std::endl;
+        std::cout << "Trainer 2 storage: " << sizeof(dll::base_cd_trainer<1, cdbn_t::rbm_type<2>, false, false>) / mini_div << "MB necessary" << std::endl;
+
+        std::cout << "Final test features:" << test_image_names.size() * cdbn->output_size() * div << "MB" << std::endl;
+
+        cdbn->template layer<0>().learning_rate /= 10;
+        //cdbn->template layer<1>()->learning_rate *= 10;
+
+        cdbn->pretrain(training_images, 10);
+        cdbn->store("method_1_half.dat");
+        //cdbn->load("method_1_half.dat");
+
+        evaluate(cdbn);
     } else {
         using crbm_t =
             dll::conv_rbm_desc<
@@ -274,18 +341,20 @@ int command_train(const config& conf){
             , dll::parallel
             , dll::verbose
             , dll::momentum
+            , dll::weight_type<weight>
             , dll::weight_decay<dll::decay_type::L2>
             >::rbm_t;
 
-        auto crbm = std::make_unique<crbm_t>();
+        //auto crbm = std::make_unique<crbm_t>();
 
-        std::cout << crbm->output_size() << " output features" << std::endl;
+        //std::cout << crbm->output_size() << " output features" << std::endl;
+        //std::cout << ((train_image_names.size() * crbm->output_size() * sizeof(std::size_t)) / 1024 / 1024) << "MB necessary" << std::endl;
 
-        crbm->train(training_images, 5);
-        crbm->store("method_1.dat");
+        //crbm->train(training_images, 5);
+        //crbm->store("method_1.dat");
         //crbm->load("method_1.dat");
 
-        evaluate(crbm);
+        //evaluate(crbm);
     }
 
     return 0;
