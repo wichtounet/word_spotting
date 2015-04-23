e638730f624013f4bff1a0ec63d80905460be37a
Baptiste Wicht <baptiste.wicht@gmail.com>
2015-04-23 16:16:16 +0200

    Robust evaluation method

--- src/spotter.cpp
+++ src/spotter.cpp
@@ -5,8 +5,10 @@
 //  http://opensource.org/licenses/MIT)
 //=======================================================================
 
-
 #include <iostream>
+#include <fstream>
+
+#include <sys/stat.h>
 
 #include "dll/conv_rbm.hpp"
 #include "dll/conv_rbm_mp.hpp"
@@ -81,6 +83,267 @@ etl::dyn_matrix<weight> mat_to_dyn(const config& conf, const cv::Mat& image){
     }
 
     return training_image;
+}
+
+template<typename Dataset, typename Set, typename DBN>
+void evaluate_patches_andreas(const Dataset& dataset, const Set& set, const config& conf, const DBN& dbn, std::size_t patches, const std::vector<std::string>& train_word_names, const std::vector<std::string>& test_image_names){
+    std::cout << "Select a folder ..." << std::endl;
+
+    mkdir("./results", 0777);
+
+    std::size_t result_name = 0;
+
+    std::string result_folder;
+
+    struct stat buffer;
+    do {
+        ++result_name;
+        result_folder = "./results/" + std::to_string(result_name);
+    } while(stat(result_folder.c_str(), &buffer) == 0);
+
+    mkdir(result_folder.c_str(), 0777);
+
+    std::cout << "... " << result_folder << std::endl;
+
+    //Get some sizes
+
+    const std::size_t patch_height = HEIGHT / conf.downscale;
+    const std::size_t patch_width = conf.patch_width;
+    const std::size_t stride = conf.patch_stride;
+
+    {
+        std::cout << "Generate relevance files..." << std::endl;
+
+        std::ofstream global_relevance_stream(result_folder + "/global_rel_file");
+        std::ofstream local_relevance_stream(result_folder + "/local_rel_file");
+
+        for(std::size_t k = 0; k < set.keywords.size(); ++k){
+            auto& keyword = set.keywords[k];
+
+            for(std::size_t t = 0; t < test_image_names.size(); ++t){
+                decltype(auto) test_image = test_image_names[t];
+
+                std::string keyword_str;
+                keyword_str = std::accumulate(keyword.begin(), keyword.end(), keyword_str);
+
+                global_relevance_stream << "cv1 0 " << keyword_str << "_" << test_image;
+                local_relevance_stream << "cv1_" << keyword_str << " 0 " << test_image;
+
+                if(dataset.word_labels.at({test_image.begin(), test_image.end() - 4}) == keyword){
+                    global_relevance_stream << " 1" << std::endl;
+                    local_relevance_stream << " 1" << std::endl;
+                } else {
+                    global_relevance_stream << " 0" << std::endl;
+                    local_relevance_stream << " 0" << std::endl;
+                }
+            }
+        }
+
+        std::cout << "... done" << std::endl;
+    }
+
+    std::cout << "Prepare the outputs ..." << std::endl;
+
+    std::vector<std::vector<etl::dyn_matrix<weight, 3>>> test_features_a;
+
+    for(std::size_t i = 0; i < test_image_names.size(); ++i){
+        std::vector<etl::dyn_matrix<weight, 3>> vec;
+
+        for(std::size_t p = 0; p < patches; ++p){
+            vec.emplace_back(dbn.prepare_one_output());
+        }
+
+       test_features_a.push_back(std::move(vec));
+    }
+
+    cpp::default_thread_pool<> pool;
+
+    cpp::parallel_foreach_i(pool, test_image_names.begin(), test_image_names.end(),
+        [&,patch_height,patch_width](auto& test_image, std::size_t i){
+            auto image = mat_to_dyn(conf, dataset.word_images.at(test_image));
+
+            for(std::size_t p = 0; p < patches; ++p){
+                etl::dyn_matrix<weight> patch(patch_height, patch_width);
+
+                for(std::size_t y = 0; y < etl::dim<0>(patch); ++y){
+                    for(std::size_t x = 0; x < etl::dim<1>(patch); ++x){
+                        patch(y, x) = image(y, x + p * conf.patch_stride);
+                    }
+                }
+
+                dbn.activation_probabilities(patch, test_features_a[i][p]);
+            }
+        });
+
+    std::cout << "... done" << std::endl;
+
+    std::cout << "Evaluate performance..." << std::endl;
+
+    std::size_t evaluated = 0;
+
+    std::vector<double> eer(set.keywords.size());
+    std::vector<double> ap(set.keywords.size());
+
+    std::ofstream global_top_stream(result_folder + "/global_top_file");
+    std::ofstream local_top_stream(result_folder + "/local_top_file");
+
+    for(std::size_t k = 0; k < set.keywords.size(); ++k){
+        auto& keyword = set.keywords[k];
+
+        std::string training_image;
+        for(auto& labels : dataset.word_labels){
+            if(keyword == labels.second && std::find(train_word_names.begin(), train_word_names.end(), labels.first) != train_word_names.end()){
+                training_image = labels.first;
+                break;
+            }
+        }
+
+        //Make sure that there is a sample in the training set
+        if(training_image.empty()){
+            std::cout << "Skipped " << keyword << " since there are no example in the training set" << std::endl;
+            continue;
+        }
+
+        auto total_positive = std::count_if(test_image_names.begin(), test_image_names.end(),
+            [&dataset, &keyword](auto& i){ return dataset.word_labels.at({i.begin(), i.end() - 4}) == keyword; });
+
+        //Make sure that there is a sample in the test set
+        if(total_positive == 0){
+            std::cout << "Skipped " << keyword << " since there are no example in the test set" << std::endl;
+            continue;
+        }
+
+        ++evaluated;
+
+        auto image = mat_to_dyn(conf, dataset.word_images.at(training_image + ".png"));
+
+        std::vector<etl::dyn_matrix<weight, 3>> ref_a;
+
+        for(std::size_t i = 0; i < patches; ++i){
+            ref_a.push_back(dbn.prepare_one_output());
+        }
+
+        for(std::size_t p = 0; p < patches; ++p){
+            etl::dyn_matrix<weight> patch(patch_height, patch_width);
+
+            for(std::size_t y = 0; y < patch_height; ++y){
+                for(std::size_t x = 0; x < patch_width; ++x){
+                    patch(y, x) = image(y, x + p * stride);
+                }
+            }
+
+            dbn.activation_probabilities(patch, ref_a[p]);
+        }
+
+        std::vector<std::pair<std::string, weight>> diffs_a;
+
+        for(std::size_t t = 0; t < test_image_names.size(); ++t){
+            decltype(auto) test_image = test_image_names[t];
+
+            double diff_a = 0;
+
+            for(std::size_t p = 0; p < patches; ++p){
+                diff_a += std::sqrt(etl::sum((ref_a[p] - test_features_a[t][p]) >> (ref_a[p] - test_features_a[t][p])));
+            }
+
+            diffs_a.emplace_back(std::string(test_image.begin(), test_image.end() - 4), diff_a);
+        }
+
+        std::sort(diffs_a.begin(), diffs_a.end(), [](auto& a, auto& b){ return a.second < b.second; });
+
+        std::vector<std::size_t> tp(diffs_a.size());
+        std::vector<std::size_t> fp(diffs_a.size());
+        std::vector<std::size_t> fn(diffs_a.size());
+        std::vector<double> tpr(diffs_a.size());
+        std::vector<double> fpr(diffs_a.size());
+        std::vector<double> recall(diffs_a.size());
+        std::vector<double> precision(diffs_a.size());
+
+        std::size_t ap_updates = 0;
+
+        for(std::size_t n = 0; n < diffs_a.size(); ++n){
+            std::string keyword_str;
+            keyword_str = std::accumulate(keyword.begin(), keyword.end(), keyword_str);
+
+            global_top_stream << "cv1 Q0 " << keyword_str << "_" << diffs_a[n].first << ".png 0 " << -diffs_a[n].second << " bw" << std::endl;
+            local_top_stream << "cv1_" << keyword_str << " Q0 " << diffs_a[n].first << ".png 0 " << -diffs_a[n].second << " bw" << std::endl;
+
+            std::size_t tp_n = n == 0 ? 0 : tp[n - 1];
+            std::size_t fp_n = n == 0 ? 0 : fp[n - 1];
+            std::size_t fn_n = n == 0 ? total_positive : fn[n - 1];
+
+            if(dataset.word_labels.at(diffs_a[n].first) == keyword){
+                ++tp_n;
+                --fn_n;
+            } else {
+                ++fp_n;
+            }
+
+            tp[n] = tp_n;
+            fp[n] = fp_n;
+            fn[n] = fn_n;
+
+            tpr[n] = static_cast<double>(tp_n) / (tp_n + fn_n);
+            fpr[n] = static_cast<double>(fp_n) / (n + 1);
+
+            recall[n] = tpr[n];
+            precision[n] = static_cast<double>(tp_n) / (tp_n + fp_n);
+
+            if(std::abs(fpr[n] - (1.0 - tpr[n])) < 1e-7){
+                eer[k] = fpr[n];
+            }
+
+            if(n == 0){
+                ++ap_updates;
+                ap[k] += precision[n];
+            } else if(recall[n] != recall[n - 1]){
+                ++ap_updates;
+                ap[k] += precision[n];
+            }
+        }
+
+        ap[k] /= ap_updates;
+
+        std::ofstream roc_gp_stream(result_folder + "/" + std::to_string(k) + "_roc.gp");
+
+        roc_gp_stream << "set terminal png size 300,300 enhanced" << std::endl;
+        roc_gp_stream << "set output '" << k << "_roc.png'" << std::endl;
+        roc_gp_stream << "set title \"ROC(" << k << ")\"" << std::endl;
+        roc_gp_stream << "set xlabel \"FPR\"" << std::endl;
+        roc_gp_stream << "set ylabel \"TPR\"" << std::endl;
+        roc_gp_stream << "plot [0:1] '" << k << "_roc.dat' with lines title ''" << std::endl;
+
+        std::ofstream roc_data_stream(result_folder + "/" + std::to_string(k) + "_roc.dat");
+
+        for(std::size_t nn = 0; nn < tpr.size(); ++nn){
+            roc_data_stream << fpr[nn] << " " << tpr[nn] << std::endl;
+        }
+
+        std::ofstream pr_gp_stream(result_folder + "/" + std::to_string(k) + "_pr.gp");
+
+        pr_gp_stream << "set terminal png size 300,300 enhanced" << std::endl;
+        pr_gp_stream << "set output '" << k << "_pr.png'" << std::endl;
+        pr_gp_stream << "set title \"PR(" << k << ")\"" << std::endl;
+        pr_gp_stream << "set xlabel \"Recall\"" << std::endl;
+        pr_gp_stream << "set ylabel \"Precision\"" << std::endl;
+        pr_gp_stream << "plot [0:1] '" << k << "_pr.dat' with lines title ''" << std::endl;
+
+        std::ofstream pr_data_stream(result_folder + "/" + std::to_string(k) + "_pr.dat");
+
+        for(std::size_t nn = 0; nn < tpr.size(); ++nn){
+            pr_data_stream << precision[nn] << " " << recall[nn] << std::endl;
+        }
+    }
+
+    std::cout << "... done" << std::endl;
+
+    std::cout << evaluated << " keywords evaluated" << std::endl;
+
+    double mean_eer = std::accumulate(eer.begin(), eer.end(), 0.0) / eer.size();
+    double mean_ap = std::accumulate(ap.begin(), ap.end(), 0.0) / ap.size();
+
+    std::cout << "Mean EER: " << mean_eer << std::endl;
+    std::cout << "Mean AP: " << mean_ap << std::endl;
 }
 
 template<typename Dataset, typename Set, typename DBN>
@@ -913,10 +1176,10 @@ int command_train(config& conf){
             //cdbn->load(file_name);
 
             std::cout << "Evaluate on training set" << std::endl;
-            evaluate_patches(dataset, set, conf, *cdbn, patches, train_word_names, train_image_names);
+            evaluate_patches_andreas(dataset, set, conf, *cdbn, patches, train_word_names, train_image_names);
 
             std::cout << "Evaluate on test set" << std::endl;
-            evaluate_patches(dataset, set, conf, *cdbn, patches, train_word_names, test_image_names);
+            evaluate_patches_andreas(dataset, set, conf, *cdbn, patches, train_word_names, test_image_names);
         } else {
             std::cout << "error: Only -third resolution is supported in method 2 for now" << std::endl;
             print_usage();
