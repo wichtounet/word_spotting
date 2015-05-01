7c09dd081ce313a366d9a38689593b74903523a2
Baptiste Wicht <baptiste.wicht@gmail.com>
2015-05-01 18:02:54 +0200

    Try standard method

--- src/spotter.cpp
+++ src/spotter.cpp
@@ -120,6 +120,95 @@ std::vector<etl::dyn_matrix<weight>> mat_to_patches(const config& conf, const cv
     return patches;
 }
 
+std::vector<etl::dyn_vector<weight>> standard_features(const config& conf, const cv::Mat& image){
+    cv::Mat clean_image;
+
+    cv::Mat scaled_normalized(cv::Size(image.size().width / conf.downscale, image.size().height / conf.downscale), CV_8U);
+    cv::resize(image, scaled_normalized, scaled_normalized.size(), cv::INTER_AREA);
+    cv::adaptiveThreshold(scaled_normalized, clean_image, 255, CV_ADAPTIVE_THRESH_MEAN_C, CV_THRESH_BINARY, 7, 2);
+
+    std::vector<etl::dyn_vector<weight>> features;
+
+    for(std::size_t i = 0; i < static_cast<std::size_t>(clean_image.size().width); ++i){
+        features.emplace_back(6);
+
+        auto& f = features.back();
+
+        double lower = 0.0;
+        for(std::size_t y = 0; y < static_cast<std::size_t>(clean_image.size().height); ++y){
+            if(clean_image.at<uint8_t>(y, i) == 0){
+                lower = y;
+                break;
+            }
+        }
+
+        double upper = 0.0;
+        for(std::size_t y = clean_image.size().height; y > 0; --y){
+            if(clean_image.at<uint8_t>(y, i) == 0){
+                upper = y;
+                break;
+            }
+        }
+
+        std::size_t black = 0;
+        for(std::size_t y = clean_image.size().height; y > 0; --y){
+            if(clean_image.at<uint8_t>(y, i) == 0){
+                ++black;
+            }
+        }
+
+        std::size_t transitions = 0;
+        for(std::size_t y = 1; y < static_cast<std::size_t>(clean_image.size().height); ++y){
+            if(clean_image.at<uint8_t>(y-1, i) != clean_image.at<uint8_t>(y, i)){
+                ++transitions;
+            }
+        }
+
+        f[0] = lower;
+        f[1] = upper;
+        f[2] = static_cast<double>(black) / clean_image.size().height ;
+        f[3] = transitions;
+        f[4] = 0.0;
+        f[5] = 0.0;
+    }
+
+    const auto width = static_cast<std::size_t>(clean_image.size().width);
+
+    for(std::size_t i = 0; i < width - 1; ++i){
+        auto& f = features[i];
+
+        f[4] = f[0] - features[i+1][0];
+        f[5] = f[1] - features[i+1][1];
+    }
+
+    for(std::size_t f = 0; f < 6; ++f){
+        // Compute the mean
+        double mean = 0.0;
+        for(std::size_t i = 0; i < width; ++i){
+            mean += features[i][f];
+        }
+        mean /= width;
+        //Normalize to zero-mean
+        for(std::size_t i = 0; i < width; ++i){
+            features[i][f] -= mean;
+        }
+        //Compute the variance
+        double std = 0.0;
+        for(std::size_t i = 0; i < width; ++i){
+            std += features[i][f] * features[i][f];
+        }
+        std = std::sqrt(std / width);
+        //Normalize to unit variance
+        if(std != 0.0){
+            for(std::size_t i = 0; i < width; ++i){
+                features[i][f] /= std;
+            }
+        }
+    }
+
+    return features;
+}
+
 template<typename V1, typename V2>
 double dtw_distance(const V1& s, const V2& t){
     const auto n = s.size();
@@ -147,6 +236,205 @@ double dtw_distance(const V1& s, const V2& t){
     }
 
     return dtw(n, m);
+}
+
+template<typename Dataset, typename Set>
+void evaluate_dtw(const Dataset& dataset, const Set& set, const config& conf, const std::vector<std::string>& train_word_names, const std::vector<std::string>& test_image_names){
+    std::cout << "Select a folder ..." << std::endl;
+
+    mkdir("./dtw_results", 0777);
+
+    std::size_t result_name = 0;
+
+    std::string result_folder;
+
+    struct stat buffer;
+    do {
+        ++result_name;
+        result_folder = "./dtw_results/" + std::to_string(result_name);
+    } while(stat(result_folder.c_str(), &buffer) == 0);
+
+    mkdir(result_folder.c_str(), 0777);
+
+    std::cout << "... " << result_folder << std::endl;
+
+    {
+        std::cout << "Generate relevance files..." << std::endl;
+
+        std::ofstream global_relevance_stream(result_folder + "/global_rel_file");
+        std::ofstream local_relevance_stream(result_folder + "/local_rel_file");
+
+        for(std::size_t k = 0; k < set.keywords.size(); ++k){
+            auto& keyword = set.keywords[k];
+
+            for(std::size_t t = 0; t < test_image_names.size(); ++t){
+                decltype(auto) test_image = test_image_names[t];
+
+                std::string keyword_str;
+                keyword_str = std::accumulate(keyword.begin(), keyword.end(), keyword_str);
+
+                global_relevance_stream << "cv1 0 " << keyword_str << "_" << test_image;
+                local_relevance_stream << "cv1_" << keyword_str << " 0 " << test_image;
+
+                if(dataset.word_labels.at({test_image.begin(), test_image.end() - 4}) == keyword){
+                    global_relevance_stream << " 1" << std::endl;
+                    local_relevance_stream << " 1" << std::endl;
+                } else {
+                    global_relevance_stream << " 0" << std::endl;
+                    local_relevance_stream << " 0" << std::endl;
+                }
+            }
+        }
+
+        std::cout << "... done" << std::endl;
+    }
+
+    std::cout << "Evaluate performance..." << std::endl;
+
+    std::size_t evaluated = 0;
+
+    std::vector<double> eer(set.keywords.size());
+    std::vector<double> ap(set.keywords.size());
+
+    std::ofstream global_top_stream(result_folder + "/global_top_file");
+    std::ofstream local_top_stream(result_folder + "/local_top_file");
+
+    for(std::size_t k = 0; k < set.keywords.size(); ++k){
+        auto& keyword = set.keywords[k];
+
+        std::string training_image;
+        for(auto& labels : dataset.word_labels){
+            if(keyword == labels.second && std::find(train_word_names.begin(), train_word_names.end(), labels.first) != train_word_names.end()){
+                training_image = labels.first;
+                break;
+            }
+        }
+
+        //Make sure that there is a sample in the training set
+        if(training_image.empty()){
+            std::cout << "Skipped " << keyword << " since there are no example in the training set" << std::endl;
+            continue;
+        }
+
+        auto total_positive = std::count_if(test_image_names.begin(), test_image_names.end(),
+            [&dataset, &keyword](auto& i){ return dataset.word_labels.at({i.begin(), i.end() - 4}) == keyword; });
+
+        //Make sure that there is a sample in the test set
+        if(total_positive == 0){
+            std::cout << "Skipped " << keyword << " since there are no example in the test set" << std::endl;
+            continue;
+        }
+
+        ++evaluated;
+
+        auto ref_a = standard_features(conf, dataset.word_images.at(training_image + ".png"));
+
+        std::vector<std::pair<std::string, weight>> diffs_a;
+
+        for(std::size_t t = 0; t < test_image_names.size(); ++t){
+            decltype(auto) test_image = test_image_names[t];
+
+            auto test_a = standard_features(conf, dataset.word_images.at(test_image));
+
+            double diff_a = dtw_distance(ref_a, test_a);
+            diffs_a.emplace_back(std::string(test_image.begin(), test_image.end() - 4), diff_a);
+        }
+
+        std::sort(diffs_a.begin(), diffs_a.end(), [](auto& a, auto& b){ return a.second < b.second; });
+
+        std::vector<std::size_t> tp(diffs_a.size());
+        std::vector<std::size_t> fp(diffs_a.size());
+        std::vector<std::size_t> fn(diffs_a.size());
+        std::vector<double> tpr(diffs_a.size());
+        std::vector<double> fpr(diffs_a.size());
+        std::vector<double> recall(diffs_a.size());
+        std::vector<double> precision(diffs_a.size());
+
+        std::size_t ap_updates = 0;
+
+        for(std::size_t n = 0; n < diffs_a.size(); ++n){
+            std::string keyword_str;
+            keyword_str = std::accumulate(keyword.begin(), keyword.end(), keyword_str);
+
+            global_top_stream << "cv1 Q0 " << keyword_str << "_" << diffs_a[n].first << ".png 0 " << -diffs_a[n].second << " bw" << std::endl;
+            local_top_stream << "cv1_" << keyword_str << " Q0 " << diffs_a[n].first << ".png 0 " << -diffs_a[n].second << " bw" << std::endl;
+
+            std::size_t tp_n = n == 0 ? 0 : tp[n - 1];
+            std::size_t fp_n = n == 0 ? 0 : fp[n - 1];
+            std::size_t fn_n = n == 0 ? total_positive : fn[n - 1];
+
+            if(dataset.word_labels.at(diffs_a[n].first) == keyword){
+                ++tp_n;
+                --fn_n;
+            } else {
+                ++fp_n;
+            }
+
+            tp[n] = tp_n;
+            fp[n] = fp_n;
+            fn[n] = fn_n;
+
+            tpr[n] = static_cast<double>(tp_n) / (tp_n + fn_n);
+            fpr[n] = static_cast<double>(fp_n) / (n + 1);
+
+            recall[n] = tpr[n];
+            precision[n] = static_cast<double>(tp_n) / (tp_n + fp_n);
+
+            if(std::abs(fpr[n] - (1.0 - tpr[n])) < 1e-7){
+                eer[k] = fpr[n];
+            }
+
+            if(n == 0){
+                ++ap_updates;
+                ap[k] += precision[n];
+            } else if(recall[n] != recall[n - 1]){
+                ++ap_updates;
+                ap[k] += precision[n];
+            }
+        }
+
+        ap[k] /= ap_updates;
+
+        std::ofstream roc_gp_stream(result_folder + "/" + std::to_string(k) + "_roc.gp");
+
+        roc_gp_stream << "set terminal png size 300,300 enhanced" << std::endl;
+        roc_gp_stream << "set output '" << k << "_roc.png'" << std::endl;
+        roc_gp_stream << "set title \"ROC(" << k << ")\"" << std::endl;
+        roc_gp_stream << "set xlabel \"FPR\"" << std::endl;
+        roc_gp_stream << "set ylabel \"TPR\"" << std::endl;
+        roc_gp_stream << "plot [0:1] '" << k << "_roc.dat' with lines title ''" << std::endl;
+
+        std::ofstream roc_data_stream(result_folder + "/" + std::to_string(k) + "_roc.dat");
+
+        for(std::size_t nn = 0; nn < tpr.size(); ++nn){
+            roc_data_stream << fpr[nn] << " " << tpr[nn] << std::endl;
+        }
+
+        std::ofstream pr_gp_stream(result_folder + "/" + std::to_string(k) + "_pr.gp");
+
+        pr_gp_stream << "set terminal png size 300,300 enhanced" << std::endl;
+        pr_gp_stream << "set output '" << k << "_pr.png'" << std::endl;
+        pr_gp_stream << "set title \"PR(" << k << ")\"" << std::endl;
+        pr_gp_stream << "set xlabel \"Recall\"" << std::endl;
+        pr_gp_stream << "set ylabel \"Precision\"" << std::endl;
+        pr_gp_stream << "plot [0:1] '" << k << "_pr.dat' with lines title ''" << std::endl;
+
+        std::ofstream pr_data_stream(result_folder + "/" + std::to_string(k) + "_pr.dat");
+
+        for(std::size_t nn = 0; nn < tpr.size(); ++nn){
+            pr_data_stream << precision[nn] << " " << recall[nn] << std::endl;
+        }
+    }
+
+    std::cout << "... done" << std::endl;
+
+    std::cout << evaluated << " keywords evaluated" << std::endl;
+
+    double mean_eer = std::accumulate(eer.begin(), eer.end(), 0.0) / eer.size();
+    double mean_ap = std::accumulate(ap.begin(), ap.end(), 0.0) / ap.size();
+
+    std::cout << "Mean EER: " << mean_eer << std::endl;
+    std::cout << "Mean AP: " << mean_ap << std::endl;
 }
 
 template<typename Dataset, typename Set, typename DBN>
@@ -605,7 +893,15 @@ int command_train(config& conf){
     std::cout << valid_image_names.size() << " validation word images in set" << std::endl;
     std::cout << test_image_names.size() << " test word images in set" << std::endl;
 
-    if(conf.method_1){
+    if(conf.method_0){
+        std::cout << "Use method 0 (Standard Features + DTW)" << std::endl;
+
+        std::cout << "Evaluate on training set" << std::endl;
+        evaluate_dtw(dataset, set, conf, train_word_names, train_image_names);
+
+        std::cout << "Evaluate on test set" << std::endl;
+        evaluate_dtw(dataset, set, conf, train_word_names, test_image_names);
+    } else if(conf.method_1){
         std::vector<etl::dyn_matrix<weight>> training_images;
 
         for(auto& name : train_image_names){
@@ -1338,7 +1634,7 @@ int main(int argc, char** argv){
 
     auto conf = parse_args(argc, argv);
 
-    if(!conf.method_1 && !conf.method_2){
+    if(!conf.method_0 && !conf.method_1 && !conf.method_2){
         std::cout << "error: One method must be selected" << std::endl;
         print_usage();
         return -1;
