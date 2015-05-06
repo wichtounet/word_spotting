6d1318dd4817ccc64429a1a4dde31ff4457cdccc
Baptiste Wicht <baptiste.wicht@gmail.com>
2015-05-06 09:31:41 +0200

    Let's try

--- include/config_third.hpp
+++ include/config_third.hpp
@@ -19,26 +19,26 @@ constexpr const std::size_t height = 40;        //Should not be changed
 constexpr const std::size_t width = 220;        //Should not be changed
 
 constexpr const std::size_t patch_height = 40;  //Should not be changed
-constexpr const std::size_t patch_width = 20;   //Should not be changed
+constexpr const std::size_t patch_width = 20;
 
 constexpr const std::size_t epochs = 10;
 constexpr const std::size_t patch_stride = 10;
 
 constexpr const std::size_t NF1 = 9;
-constexpr const std::size_t K1 = 48;
+constexpr const std::size_t K1 = 40;
 constexpr const std::size_t C1 = 2;
 constexpr const std::size_t B1 = 25;
 constexpr const dll::unit_type HT1 = dll::unit_type::BINARY;
 constexpr const dll::decay_type DT1 = dll::decay_type::L2;
-constexpr const dll::sparsity_method SM1 = dll::sparsity_method::NONE;
+constexpr const dll::sparsity_method SM1 = dll::sparsity_method::LEE;
 
-constexpr const std::size_t NF2 = 5;
-constexpr const std::size_t K2 = 48;
+constexpr const std::size_t NF2 = 3;
+constexpr const std::size_t K2 = 40;
 constexpr const std::size_t C2 = 2;
 constexpr const std::size_t B2 = 25;
 constexpr const dll::unit_type HT2 = dll::unit_type::BINARY;
 constexpr const dll::decay_type DT2 = dll::decay_type::L2;
-constexpr const dll::sparsity_method SM2 = dll::sparsity_method::NONE;
+constexpr const dll::sparsity_method SM2 = dll::sparsity_method::LEE;
 
 constexpr const std::size_t NF3 = 3;
 constexpr const std::size_t K3 = 48;
