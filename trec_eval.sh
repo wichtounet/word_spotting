71cd01cd5d4488447cb38b72961911f8bc51272f
Baptiste Wicht <baptiste.wicht@gmail.com>
2015-05-20 15:59:24 +0200

    Improve the script

--- trec_eval.sh
+++ trec_eval.sh
@@ -2,10 +2,10 @@
 
 folder=$1
 
-gmap=`/home/wichtounet/dev/trec_eval/trec_eval -q $folder/global_rel_file $folder/global_top_file | /usr/bin/zgrep "map.*all" | cut -f3`
-grp=`/home/wichtounet/dev/trec_eval/trec_eval -q $folder/global_rel_file $folder/global_top_file | /usr/bin/zgrep "\(R-prec\).*all" | cut -f3`
-lmap=`/home/wichtounet/dev/trec_eval/trec_eval -q $folder/local_rel_file $folder/local_top_file | /usr/bin/zgrep "map.*all" | cut -f3`
-lrp=`/home/wichtounet/dev/trec_eval/trec_eval -q $folder/local_rel_file $folder/local_top_file | /usr/bin/zgrep "\(R-prec\).*all" | cut -f3`
+gmap=`/home/wichtounet/dev/trec_eval/trec_eval -q $folder/global_rel_file $folder/global_top_file | /usr/bin/zgrep "map\s*all" | cut -f3`
+grp=`/home/wichtounet/dev/trec_eval/trec_eval -q $folder/global_rel_file $folder/global_top_file | /usr/bin/zgrep "\(R-prec\)\s*all" | cut -f3`
+lmap=`/home/wichtounet/dev/trec_eval/trec_eval -q $folder/local_rel_file $folder/local_top_file | /usr/bin/zgrep "map\s*all" | cut -f3`
+lrp=`/home/wichtounet/dev/trec_eval/trec_eval -q $folder/local_rel_file $folder/local_top_file | /usr/bin/zgrep "\(R-prec\)\s*all" | cut -f3`
 
 echo "G-MAP: $gmap"
 echo "G-RP:  $grp"
