6ab94b21c585f3ed96e51c67d4c63d91b9002ac7
Baptiste Wicht <baptiste.wicht@gmail.com>
2016-01-12 13:51:17 +0100

    Prepare computation of the features suffix

--- src/evaluate_features.cpp
+++ src/evaluate_features.cpp
@@ -10,8 +10,33 @@
 
 #include "memory.hpp"            //First for debug reasons
 #include "evaluate_features.hpp" //The header of this source file
+#include "patches_config.hpp"    //The different configurations
+
+#define SUFFIX .
+#define SUFFIX_CAT_I(A, B) A ## B
+#define SUFFIX_CAT(A, B) SUFFIX_CAT_I(A, B)
+#define STRINGIFY_I(A) #A
+#define STRINGIFY(A) STRINGIFY_I(A)
 
 namespace {
+
+std::string get_suffix(config& conf){
+    if(conf.method_0){
+        return ".0";
+    } else if(conf.method_1){
+        return ".1";
+    } else if(conf.method_2){
+        if(conf.half){
+            return STRINGIFY(SUFFIX_CAT(SUFFIX, HALF_LEVELS));
+        } else if(conf.third){
+            return STRINGIFY(SUFFIX_CAT(SUFFIX, THIRD_LEVELS));
+        } else {
+            return STRINGIFY(SUFFIX_CAT(SUFFIX, FULL_LEVELS));
+        }
+    }
+
+    return ".invalid";
+}
 
 } // end of anonymous namespace
 
