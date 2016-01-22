87c58b2d871aaf37b087878b45f41eaa5c8f6f55
Baptiste Wicht <baptiste.wicht@gmail.com>
2016-01-22 12:48:35 +0100

    Update copyright

--- src/evaluate_features.cpp
+++ src/evaluate_features.cpp
@@ -1,5 +1,5 @@
 //=======================================================================
-// Copyright Baptiste Wicht 2015.
+// Copyright Baptiste Wicht 2015-2016.
 // Distributed under the MIT License.
 // (See accompanying file LICENSE or copy at
 //  http://opensource.org/licenses/MIT)
