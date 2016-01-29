87b3b5e4a2661b2b5824c095b7b972bba2db8d4e
Baptiste Wicht <baptiste.wicht@gmail.com>
2016-01-29 07:32:47 +0100

    Restore half configuration

--- include/config_half.hpp
+++ include/config_half.hpp
@@ -28,18 +28,20 @@ constexpr const std::size_t test_stride  = 2;
 constexpr const std::size_t NF1          = 9;
 constexpr const std::size_t K1           = 12;
 constexpr const std::size_t C1           = 2;
-constexpr const std::size_t B1           = 50;
-constexpr const dll::unit_type HT1       = dll::unit_type::RELU;
+constexpr const std::size_t B1           = 64;
+constexpr const dll::unit_type HT1       = dll::unit_type::BINARY;
 constexpr const dll::decay_type DT1      = dll::decay_type::L2;
-constexpr const dll::sparsity_method SM1 = dll::sparsity_method::NONE;
+constexpr const dll::sparsity_method SM1 = dll::sparsity_method::LEE;
+constexpr const bool shuffle_1           = true;
 
 constexpr const std::size_t NF2          = 3;
 constexpr const std::size_t K2           = 12;
 constexpr const std::size_t C2           = 2;
-constexpr const std::size_t B2           = 50;
-constexpr const dll::unit_type HT2       = dll::unit_type::RELU;
+constexpr const std::size_t B2           = 64;
+constexpr const dll::unit_type HT2       = dll::unit_type::BINARY;
 constexpr const dll::decay_type DT2      = dll::decay_type::L2;
-constexpr const dll::sparsity_method SM2 = dll::sparsity_method::NONE;
+constexpr const dll::sparsity_method SM2 = dll::sparsity_method::LEE;
+constexpr const bool shuffle_2           = true;
 
 constexpr const std::size_t NF3          = 3;
 constexpr const std::size_t K3           = 48;
@@ -47,10 +49,11 @@ constexpr const std::size_t C3           = 2;
 constexpr const std::size_t B3           = 25;
 constexpr const dll::unit_type HT3       = dll::unit_type::BINARY;
 constexpr const dll::decay_type DT3      = dll::decay_type::L2;
-constexpr const dll::sparsity_method SM3 = dll::sparsity_method::NONE;
+constexpr const dll::sparsity_method SM3 = dll::sparsity_method::LEE;
+constexpr const bool shuffle_3           = true;
 
-const auto rate_0 = [](weight& value) { value = 1.0 * value; };
-const auto rate_1 = [](weight& value) { value = 1.0 * value; };
+const auto rate_0 = [](weight& value) { value = 0.1 * value; };
+const auto rate_1 = [](weight& value) { value = 0.1 * value; };
 const auto rate_2 = [](weight& value) { value = 1.0 * value; };
 
 const auto momentum_0 = [](weight& ini, weight& fin) { ini = 0.9; fin = 0.9; };
@@ -65,8 +68,8 @@ const auto wd_l2_0 = [](weight& value) { value = 1.0 * value; };
 const auto wd_l2_1 = [](weight& value) { value = 1.0 * value; };
 const auto wd_l2_2 = [](weight& value) { value = 1.0 * value; };
 
-const auto pbias_0 = [](weight& value) { value = 0.5 * value; };
-const auto pbias_1 = [](weight& value) { value = 0.5 * value; };
+const auto pbias_0 = [](weight& value) { value = 1.0 * value; };
+const auto pbias_1 = [](weight& value) { value = 1.0 * value; };
 const auto pbias_2 = [](weight& value) { value = 1.0 * value; };
 
 const auto pbias_lambda_0 = [](weight& value) { value = 2.0 * value; };
