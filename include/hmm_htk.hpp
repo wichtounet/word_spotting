07c0e1be2bd92c2c45be85e27aacc53ac09ebd94
Baptiste Wicht <baptiste.wicht@gmail.com>
2016-03-16 11:32:07 +0100

    Prepare HTK integration

--- include/hmm_htk.hpp
+++ include/hmm_htk.hpp
@@ -18,13 +18,40 @@
 
 #include <random>
 
+#include <sys/stat.h> //mkdir
+
+#include "dll/util/io.hpp" //TODO This should be extracted to cpp_utils
+
 namespace hmm_htk {
 
 using gmm_p = std::string;
 using hmm_p = std::string;
 
+//Number of gaussians for the HMM
+static constexpr const std::size_t n_hmm_gaussians = 2;
+
+//Number of training iterations for the HMM
+static constexpr const std::size_t n_hmm_iterations = 2;
+
 //Number of states per character
 static constexpr const auto n_states_per_char = 10;
+
+inline auto exec_command(const std::string& command) {
+    std::stringstream output;
+
+    char buffer[1024];
+
+    FILE* stream = popen(command.c_str(), "r");
+
+    while (fgets(buffer, 1024, stream) != NULL) {
+        output << buffer;
+    }
+
+    auto status = pclose(stream);
+    auto exit_code = WEXITSTATUS(status);
+
+    return std::make_pair(exit_code, output.str());
+}
 
 template <typename RefFunctor>
 gmm_p train_global_hmm(names train_word_names, RefFunctor functor) {
@@ -56,14 +83,215 @@ template <typename Dataset, typename Ref>
 hmm_p train_ref_hmm(const Dataset& dataset, Ref& ref_a, names training_images) {
     dll::auto_timer timer("htk_hmm_train");
 
-    auto characters = dataset.word_labels.at(training_images[0]).size();
+    auto label = dataset.word_labels.at(training_images[0]);
+    auto characters = label.size();
 
     const auto n_states = characters * n_states_per_char;
     const auto n_features = ref_a[0][0].size();
 
-    //TODO
+    const std::string base_folder = ".hmm";
+    const std::string folder = base_folder + "/" + keyword_to_short_string(label);
 
-    return "frakking_hmm";
+    const std::string features_file    = folder + "/train_features.lst";
+    const std::string hmm_info_file    = folder + "/hmm_info";
+    const std::string means_file       = folder + "/means";
+    const std::string variances_file   = folder + "/variances";
+    const std::string covariances_file = folder + "/covariances";
+    const std::string init_mmf_file    = folder + "/init_mmf";
+    const std::string htk_config_file  = folder + "/htk_config";
+    const std::string letters_file     = folder + "/letters";
+    const std::string mlf_file         = folder + "/train.mlf";
+
+    const std::string bin_hmm_init = "scripts/hmm-init.pl";
+    const std::string bin_hhed   = "HHEd";
+    const std::string bin_herest = "HERest";
+
+    mkdir(base_folder.c_str(), 0777);
+    mkdir(folder.c_str(), 0777);
+
+    std::vector<std::string> features_files;
+
+    // Generate the features files (at least, it makes sense, but why binary...)
+
+    for(std::size_t i = 0; i < training_images.size(); ++i){
+        auto& image = training_images[i];
+        auto& ref   = ref_a[i];
+
+        const std::string file_path = folder + "/" + image + ".htk";
+
+        features_files.push_back(file_path);
+
+        std::ofstream os(file_path, std::ofstream::binary);
+
+        dll::binary_write(os, static_cast<int>(ref_a.size()));                 //Number of samples
+        dll::binary_write(os, static_cast<int>(1));                            //Dummy HTK_SAMPLE_RATE
+        dll::binary_write(os, static_cast<short>(n_features * sizeof(float))); //Sample size
+        dll::binary_write(os, static_cast<short>(9));                          //Used defined sample kind = 9 ?
+
+        //Write all the values
+        for(auto feature_vector : ref){
+            for(auto v : feature_vector){
+                dll::binary_write(os, v);
+            }
+        }
+    }
+
+    // Generate a file with the list of feature files (silly...)
+
+    {
+        std::ofstream os(features_file);
+
+        for(auto& file : features_files){
+            os << file << "\n";
+        }
+    }
+
+    // Generate the HTK config file (no idea what is in it)
+
+    {
+        std::ofstream os(htk_config_file);
+
+        os << "NATURALREADORDER    =   T\n";
+        os << "NATURALWRITEORDER   =   T";
+    }
+
+    // Generate a file with the states
+
+    {
+        std::ofstream os(hmm_info_file);
+
+        for(std::size_t i = 1; i <= characters; ++i){
+            os << "s" << i << " " << n_states_per_char << " nocov noinit\n";
+        }
+    }
+
+    // Generate a file with the letters
+
+    {
+        std::ofstream os(letters_file);
+
+        for(std::size_t i = 1; i <= characters; ++i){
+            os << "s" << i << "\n";
+        }
+    }
+
+    // Generate a file with the labels (I think)
+
+    {
+        std::ofstream os(mlf_file);
+
+        os << "#!MLF!#" << std::endl;
+
+        for(std::size_t i = 0; i < training_images.size(); ++i){
+            auto& image = training_images[i];
+            auto& ref   = ref_a[i];
+
+            const std::string lab_name = folder + "/" + image + ".lab";
+
+            os << "\"" << lab_name << "\"" << '\n';
+
+            for(std::size_t i = 1; i <= characters; ++i){
+                os << "s" << i << '\n';
+            }
+
+            os << "." << '\n';
+        }
+    }
+
+    // Generate the means/variances/covariances/init_mmf files
+
+    {
+        std::string init_command =
+            bin_hmm_init
+            + " --hmmmacro " + init_mmf_file
+            + " -e " + means_file
+            + " -v " + variances_file
+            + " -c " + covariances_file
+            + " --infofile " + hmm_info_file
+            + " " + features_file;
+
+        auto result = exec_command(init_command);
+
+        if(result.first){
+            std::cout << "init scripts failed with result code: " << result.first << std::endl;
+            std::cout << "Command: " << init_command << std::endl;
+            std::cout << result.second << std::endl;
+        }
+    }
+
+    for(std::size_t g = 1; g <= n_hmm_gaussians; ++g){
+        const std::string mmf_file           = folder + "/trained_" + std::to_string(g) + ".mmf";
+        const std::string stats_file         = folder + "/stats_" + std::to_string(g) + ".txt";
+        const std::string multigaussian_file = folder + "/mu_" + std::to_string(g) + ".hhed";
+
+        std::string previous_mmf_file;
+        if(g == 1){
+            previous_mmf_file = init_mmf_file;
+        } else {
+            previous_mmf_file = folder + "/trained_" + std::to_string(g - 1) + ".mmf";
+        }
+
+        std::string cp_command = "cp " + previous_mmf_file + " " + mmf_file;
+        auto cp_result = exec_command(cp_command);
+
+        if(cp_result.first){
+            std::cout << "cp failed with result code: " << cp_result.first << std::endl;
+            std::cout << "Command: " << cp_command << std::endl;
+            std::cout << cp_result.second << std::endl;
+
+            //If HHEd fails, HERest will fail anyway
+            break;
+        }
+
+        {
+            std::ofstream os(multigaussian_file);
+            os << "MU " << g << " {*.state[2-10000].mix}" << std::endl;
+        }
+
+        std::string hhed_command =
+            bin_hhed +
+            " -C " + htk_config_file +
+            " -M " + folder +
+            " -H " + mmf_file +
+            " " + multigaussian_file + " " + letters_file;
+
+        auto hhed_result = exec_command(hhed_command);
+
+        if(hhed_result.first){
+            std::cout << "HHEd failed with result code: " << hhed_result.first << std::endl;
+            std::cout << "Command: " << hhed_command << std::endl;
+            std::cout << hhed_result.second << std::endl;
+
+            //If HHEd fails, HERest will fail anyway
+            break;
+        }
+
+        for(std::size_t i = 0; i < n_hmm_iterations; ++i){
+            const double herest_min_variance = 0.000001;
+
+            std::string herest_command =
+                bin_herest +
+                " -C " + htk_config_file +
+                " -v " + std::to_string(herest_min_variance) +
+                " -M " + folder +
+                " -I " + mlf_file +
+                " -H " + mmf_file +
+                " -s " + stats_file +
+                " -S " + features_file +
+                " " + letters_file;
+
+            auto herest_result = exec_command(herest_command);
+
+            if(herest_result.first){
+                std::cout << "HERest failed with result code: " << herest_result.first << std::endl;
+                std::cout << "Command: " << herest_command << std::endl;
+                std::cout << herest_result.second << std::endl;
+            }
+
+        }
+    }
+
+    return folder;
 }
 
 template <typename Dataset, typename V1>
---
+++ scripts/hmm-init.pl
@@ -0,0 +1,250 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use File::Basename;
+use Pod::Usage;
+use Getopt::Long;
+
+my %opt = (man => 0, help => 0, hmmmacro => "hmmmacro.mmf");
+GetOptions(\%opt,'help|?|h','man|m', 'hmmmacro|o=s', 'infofile|i=s'
+, 'variances|v=s', 'covariances|c=s', 'means|e=s') or pod2usage(2);
+pod2usage(1) if $opt{help};
+pod2usage(-exitstatus => 0, -verbose => 2) if $opt{man};
+
+
+my $lshort = length( pack("s",1) );
+my $lint = length( pack("i",1) );
+my $lfloat = length( pack("f",1) );
+my $N = -1;
+my $DIM = -1;
+my @vectors = ();
+my @means = ();
+my @cov = ();
+
+# calculating the means, variances and covariances
+while(<>) {
+    chomp;
+    open FILE, $_;
+    print "$_\n";
+    binmode FILE;
+    my ($val, $num, $dummy1, $dummy2);
+    read( FILE, $num, $lint ); $num = unpack( "i", $num );
+    read( FILE, $dummy1, $lint ); $dummy1 = unpack( "i", $dummy1 );
+    read( FILE, $DIM, $lshort ); $DIM = unpack( "s", $DIM ); $DIM /= $lfloat;
+    read( FILE, $dummy2, $lshort ); $dummy2 = unpack( "s", $dummy2 );
+    if ( $N == -1 )  {  #initialize
+        for( my $j = 0; $j < $DIM; $j++ ) {
+            $means[$j] = 0;
+            for( my $k = $j; $k < $DIM; $k++ ) {
+                $cov[$j][$k] = 0;
+            }
+        }
+        $N = 0;
+    }
+    for( my $j = 0; $j < $num; $j++ ) { #read feature vectors
+        for( my $k = 0; $k < $DIM; $k++ ) {
+            read( FILE, $val, $lfloat ); $val = unpack( "f", $val );
+            $vectors[$j][$k] = $val;
+        }
+        $N++;
+    }
+    close FILE;
+    for( my $j = 0; $j < $num; $j++ ) { #accumulate mean and covariance values
+        for( my $k = 0; $k < $DIM; $k++ ) {
+            $means[$k] += $vectors[$j][$k];
+            for( my $l = $k; $l < $DIM; $l++ ) {
+                $cov[$k][$l] += $vectors[$j][$k] * $vectors[$j][$l];
+            }
+        }
+    }
+}
+
+for( my $i = 0; $i < $DIM; $i++ ) { #finalize means
+  $means[$i] /= $N;
+}
+for( my $i = 0; $i < $DIM; $i++ ) { #finalize covariances
+  for( my $j = $i; $j < $DIM; $j++ ) {
+    $cov[$i][$j] /= $N;
+    $cov[$i][$j] -= $means[$i] * $means[$j];
+  }
+}
+
+my @variances;
+for(my $i=0;$i<$DIM;$i++) {
+    push @variances, $cov[$i][$i];
+}
+
+#writing files if required
+if($opt{means}) {
+    open MEANS, ">$opt{means}" or die "could not write to $opt{means}\n";
+    print MEANS "$_\n" foreach @means;
+    close MEANS or die;
+}
+if($opt{variances}) {
+    open VARIANCES, ">$opt{variances}" or die "could not write to $opt{variances}\n";
+    print VARIANCES "$_\n" foreach @variances;
+    close VARIANCES or die;
+}
+if($opt{covariances}) {
+    open COVARIANCES, ">$opt{covariances}" or die "could not write to $opt{covariances}\n";
+    for( my $i = 0; $i < $DIM; $i++ ) {
+        for( my $j = $i; $j < $DIM; $j++ ) {
+            unless($i==$j)    {
+                print COVARIANCES " ";
+            }
+            print COVARIANCES $cov[$i][$j];
+        }
+        print COVARIANCES "\n";
+    }
+    close COVARIANCES or die;
+}
+
+# initializing the hmms
+my @names = ();
+my @states = ();
+my @cov_info = ();
+my @init = ();
+my $cov_info = 0;
+open INFO, "$opt{infofile}" or die "could not read from $opt{infofile}\n";
+while(<INFO> )
+{
+    /(\w+)\s+(\d+)\s+(cov|nocov)\s+(init|noinit)/;
+    push @names, $1;
+    push @states, $2;
+    push @cov_info, $3;
+    $cov_info = 1 if ( $3 eq "cov" );
+    push @init, $4;
+}
+close INFO;
+
+open MMF, ">$opt{hmmmacro}" or die "could not write to $opt{hmmmacro}\n";
+select MMF;
+my $vecsize = @means;
+print "~o\n<STREAMINFO> 1 $vecsize\n<VECSIZE> $vecsize<NULLD><USER>";
+print "<DIAGC>" if $cov_info == 0;
+print "\n";
+
+for(my $i = 0; $i < @names; $i++ ) {
+  print  "~h \"$names[$i]\"\n";
+  print  "<BEGINHMM>\n";
+  print  "<NUMSTATES> $states[$i]\n";
+  for( my $j = 2; $j < $states[$i]; $j++ )   {
+    print  "<STATE> $j\n";
+    print  "<MEAN> $vecsize\n";
+    for(my $k=0; $k<@means; $k++ ) {
+      if ( $init[$i] eq "noinit" ) {
+        print  " ".( 1.0 / $vecsize );
+      }
+      else {
+        print  " $means[$k]";
+      }
+    }
+    print  "\n";
+    if ( $cov_info[$i] eq "nocov" )  {
+      print  "<VARIANCE> $vecsize\n";
+      for(my $k=0; $k < @variances; $k++ )   {
+        if ( $init[$i] eq "noinit" )  {
+          print  " ".( 1.0 / $vecsize );
+        }
+        else {
+          print  " $variances[$k]";
+        }
+      }
+      print  "\n";
+    }
+    else {
+      print  "<INVCOVAR> $vecsize\n";
+      for(my $k = 0; $k < @variances; $k++ )      {
+        if ( $init[$i] eq "noinit" )   {
+          print  " ".( 1.0 / $vecsize );
+        }
+        else  {
+          print  " ".( 1.0 / $variances[$k] );
+        }
+        for( my $l = $k + 1; $l < @variances; $l++ )  {
+          print  " 0.0";
+        }
+        print  "\n";
+      }
+    }
+  }
+  print  "<TRANSP> $states[$i]\n";
+  for( my $j = 1; $j <= $states[$i]; $j++ )  {
+    for(my $k = 1; $k <= $states[$i]; $k++ )    {
+      if ( $j == 1 && $k == 2 )     {
+        print  " 1.0";
+      }
+      elsif ( $j > 1 && $j < $states[$i] && ( $k == $j || $k == $ j + 1 ) )       {
+        print  " 0.5";
+      }
+      else      {
+        print  " 0.0";
+      }
+    }
+    print  "\n";
+  }
+  print  "<ENDHMM>\n";
+}
+close MMF;
+
+
+__END__
+
+=head1 NAME
+
+inithmm - initialize the HMM macro file.
+
+=head1 SYNOPSIS
+
+inithmm [-mh] [-o hmmmacro] -i infofile trainlist
+
+=head1 DESCRPIPTION
+
+B<inithmm> extracts information (means, variances, covariances) of the feature vector file (*.htk) listes in I<trainlist> and creates
+a new HMM macro file which is written in I<hmmmacro>.
+
+=head1 OPTIONS
+
+=over 8
+
+=item B<-h, -help>
+
+Print a brief help message and exits.
+
+=item B<-m, --man>
+
+Prints the manual page and exits.
+
+=item B<-o, --hmmmacro>
+
+Specify the HMM macro file, which will contain the result (default: hmmmacro.mmf)
+
+=item B<-i, --infofile=file>
+
+Specify the HMM info file, which contains the information about the character HMMs
+A example line looks like: A 20 nocov init
+
+=item B<-e, --means=file>
+
+Print the calculates means to I<file>.
+
+=item B<-v, --variances=file>
+
+Print the calculates variances to I<file>.
+
+=item B<-c, --covariances=file>
+
+Print the calculates covariances to I<file>.
+
+=back
+
+=head1 AUTHOR
+
+Roman Bertolami 2004, bertolam@iam.unibe.ch
+
+=head1 CONTRIBUTOR
+
+Markus Wüthrich 2007-2008, kusi.w@students.unibe.ch
+
+=cut
