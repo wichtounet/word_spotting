de984859f116e6c452fdb93f8c21b484da2e624a
Baptiste Wicht <baptiste.wicht@gmail.com>
2016-04-07 16:20:47 +0200

    Prepare distribution

---
+++ include/grid.hpp
@@ -0,0 +1,15 @@
+//=======================================================================
+// Copyright Baptiste Wicht 2015-2016.
+// Distributed under the MIT License.
+// (See accompanying file LICENSE or copy at
+//  http://opensource.org/licenses/MIT)
+//=======================================================================
+
+#pragma once
+
+#include <string>
+
+struct grid_info {
+    std::string password;
+    std::vector<std::string> machines;
+};
