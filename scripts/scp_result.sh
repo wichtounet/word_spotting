81e7569e61b0a410855305da743cf838e71909e8
Baptiste Wicht <baptiste.wicht@gmail.com>
2016-04-11 16:30:11 +0200

    New download script

---
+++ scripts/scp_result.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+machine=$1
+number=$2
+
+stamp=`cat results/stamp`
+new_stamp=$((stamp+1))
+echo "$new_stamp" > results/stamp
+
+password=`cat .grid_passwd`
+
+echo "Downloading results/$number from $machine"
+sshpass -p $password scp -r wicht@$machine:/home/wicht/dev/word_spotting/results/$number results/$stamp
+echo "Results have been downloaded in results/$stamp"
