1c8540db74f40dab7d55776b3a265751d42fa980
Baptiste Wicht <baptiste.wicht@gmail.com>
2016-04-12 09:00:02 +0200

    Make more

--- scripts/scp_result.sh
+++ scripts/scp_result.sh
@@ -12,3 +12,8 @@ password=`cat .grid_passwd`
 echo "Downloading results/$number from $machine"
 sshpass -p $password scp -r wicht@$machine:/home/wicht/dev/word_spotting/results/$number results/$stamp
 echo "Results have been downloaded in results/$stamp"
+
+if [ "$3" == "trec" ]; then
+    echo "TREC evaluation of the results"
+    ./scripts/trec_eval.sh results/$stamp
+fi
