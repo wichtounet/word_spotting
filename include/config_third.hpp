f8ebed042cbc214d3508f6f2136150e1db604743
Baptiste Wicht <baptiste.wicht@gmail.com>
2016-11-22 12:34:46 +0100

    More epochs

--- include/config_third.hpp
+++ include/config_third.hpp
@@ -30,7 +30,7 @@ constexpr const std::size_t patch_width  = 20;
 // Data augmentation
 constexpr const std::size_t elastic_augment = 1; //not ready
 
-constexpr const std::size_t epochs       = 5;
+constexpr const std::size_t epochs       = 10;
 constexpr const std::size_t train_stride = 1;
 constexpr const std::size_t test_stride  = 1;
 
