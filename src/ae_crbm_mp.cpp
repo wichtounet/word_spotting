420ced1e8af6a172ca4ece507d0e17f28287e718
Baptiste Wicht <baptiste.wicht@gmail.com>
2017-02-15 16:23:41 +0100

    Set learning rates

--- src/ae_crbm_mp.cpp
+++ src/ae_crbm_mp.cpp
@@ -58,15 +58,15 @@ void crbm_mp_evaluate(const spot_dataset& dataset, const spot_dataset_set& set, 
 
 void crbm_mp_evaluate_all(const spot_dataset& dataset, const spot_dataset_set& set, config& conf, names train_word_names, names test_image_names, parameters params, const std::vector<image_t>& training_patches){
     if (conf.crbm) {
-        crbm_mp_evaluate<1>(dataset, set, conf, train_word_names, test_image_names, params, training_patches, 1e-4, epochs);
-        crbm_mp_evaluate<2>(dataset, set, conf, train_word_names, test_image_names, params, training_patches, 1e-4, epochs);
+        crbm_mp_evaluate<1>(dataset, set, conf, train_word_names, test_image_names, params, training_patches, 1e-3, epochs);
+        crbm_mp_evaluate<2>(dataset, set, conf, train_word_names, test_image_names, params, training_patches, 1e-3, epochs);
         crbm_mp_evaluate<3>(dataset, set, conf, train_word_names, test_image_names, params, training_patches, 1e-4, epochs);
-        crbm_mp_evaluate<4>(dataset, set, conf, train_word_names, test_image_names, params, training_patches, 1e-4, epochs);
-        crbm_mp_evaluate<5>(dataset, set, conf, train_word_names, test_image_names, params, training_patches, 1e-4, epochs);
-        crbm_mp_evaluate<6>(dataset, set, conf, train_word_names, test_image_names, params, training_patches, 1e-4, epochs);
-        crbm_mp_evaluate<7>(dataset, set, conf, train_word_names, test_image_names, params, training_patches, 1e-4, epochs);
-        crbm_mp_evaluate<8>(dataset, set, conf, train_word_names, test_image_names, params, training_patches, 1e-4, epochs);
-        crbm_mp_evaluate<9>(dataset, set, conf, train_word_names, test_image_names, params, training_patches, 1e-4, epochs);
-        crbm_mp_evaluate<10>(dataset, set, conf, train_word_names, test_image_names, params, training_patches, 1e-4, epochs);
+        crbm_mp_evaluate<4>(dataset, set, conf, train_word_names, test_image_names, params, training_patches, 1e-5, epochs);
+        crbm_mp_evaluate<5>(dataset, set, conf, train_word_names, test_image_names, params, training_patches, 1e-5, epochs);
+        crbm_mp_evaluate<6>(dataset, set, conf, train_word_names, test_image_names, params, training_patches, 1e-6, epochs);
+        crbm_mp_evaluate<7>(dataset, set, conf, train_word_names, test_image_names, params, training_patches, 1e-6, epochs);
+        crbm_mp_evaluate<8>(dataset, set, conf, train_word_names, test_image_names, params, training_patches, 1e-6, epochs);
+        crbm_mp_evaluate<9>(dataset, set, conf, train_word_names, test_image_names, params, training_patches, 1e-6, epochs);
+        crbm_mp_evaluate<10>(dataset, set, conf, train_word_names, test_image_names, params, training_patches, 1e-6, epochs);
     }
 }
