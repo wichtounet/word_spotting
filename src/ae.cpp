1260106fd95dfa7b66c7389fe524f8da73df8383
Baptiste Wicht <baptiste.wicht@gmail.com>
2017-02-15 09:10:02 +0100

    Integrate CRBM with PMP

--- src/ae.cpp
+++ src/ae.cpp
@@ -102,4 +102,5 @@ void ae_train(const spot_dataset& dataset, const spot_dataset_set& set, config& 
 
     crbm_evaluate_all(dataset, set, conf, train_word_names, test_image_names, params, training_patches);
     crbm_mp_evaluate_all(dataset, set, conf, train_word_names, test_image_names, params, training_patches);
+    crbm_pmp_evaluate_all(dataset, set, conf, train_word_names, test_image_names, params, training_patches);
 }
