4b03206590f43bb806e151f6de9757fef6eceb60
Baptiste Wicht <baptiste.wicht@gmail.com>
2017-02-17 08:22:13 +0100

    Reduce memory consumption

--- src/ae_stacked_conv_mp.cpp
+++ src/ae_stacked_conv_mp.cpp
@@ -56,7 +56,8 @@ void stacked_conv_mp_evaluate(const spot_dataset& dataset, const spot_dataset_se
         dll::weight_decay<dll::decay_type::L2>,
         dll::trainer<dll::sgd_trainer>,
         dll::batch_size<batch_size>,
-        dll::shuffle
+        dll::shuffle,
+        dll::batch_mode
     >::dbn_t;
 
     auto net1 = std::make_unique<network1_t>();
