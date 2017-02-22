50652bf34d05d2173851e740deb4ec623f016a2e
Baptiste Wicht <baptiste.wicht@gmail.com>
2017-02-22 16:01:05 +0100

    Use the new denoising auto function

--- src/ae_denoising_rbm.cpp
+++ src/ae_denoising_rbm.cpp
@@ -17,22 +17,6 @@
 namespace {
 
 void denoising_rbm_evaluate(double noise, const spot_dataset& dataset, const spot_dataset_set& set, config& conf, names train_word_names, names test_image_names, parameters params, const std::vector<image_t>& clean, float learning_rate, size_t epochs) {
-    auto input_transformer = [noise](auto&& value){
-        static std::random_device rd;
-        static std::default_random_engine g(rd());
-
-        std::uniform_real_distribution<double> dist(0.0, 1000.0);
-
-        for(auto& v :  value){
-            v *= dist(g) < noise * 1000.0 ? 0.0 : 1.0;
-        }
-    };
-
-    auto noisy = clean;
-    for(auto& image : noisy){
-        input_transformer(image);
-    }
-
     using network_t = dll::dbn_desc<
         dll::dbn_layers<
             dll::rbm_desc<
@@ -57,7 +41,7 @@ void denoising_rbm_evaluate(double noise, const spot_dataset& dataset, const spo
     if (noise == 0.0) {
         net->pretrain(clean, epochs);
     } else {
-        net->pretrain_denoising(noisy, clean, epochs);
+        net->pretrain_denoising_auto(clean, epochs, noise);
     }
 
     auto folder = spot::evaluate_patches_ae<0, image_t>(dataset, set, conf, *net, train_word_names, test_image_names, false, params);
